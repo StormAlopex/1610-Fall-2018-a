df01203bd2d13d4e5ae28d04953050f88335fe2f
Unknown <alopex@ymail.com>
2018-09-12 15:31:12 -0600

    Movements

---
+++ 1610/Assets/Move.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public Vector3 MoveSpeed;
+    public Vector3 RotateSpeed;
+    public float Speed;
+
+    public CharacterController Character;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        MoveSpeed.x = Speed*Input.GetAxis("Horizontal");
+        MoveSpeed *= Time.deltaTime;
+
+
+        Character.Move(MoveSpeed);
+
+  
+    }
+}
