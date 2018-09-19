da52fdd94f1ea4f43b5060d509d79286cb6bc888
Unknown <alopex@ymail.com>
2018-09-19 15:40:42 -0600

    Scriptable Objects

--- 1610/Assets/AddMoney.cs
+++ 1610/Assets/AddMoney.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AddMoney : MonoBehaviour {
+
     public int Money = 10;
 
-	// Use this for initialization
-	public void AddMoreMoney (int money) {
+
+	public void AddMoreMoney(int money)
+    {
         Money += money;
 		
 	}
 	
-	// Update is called once per frame
+	
 	void Update () {
 		
 	}
