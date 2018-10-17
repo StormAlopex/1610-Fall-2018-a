c279236db63623d40bf22978e46e8c4b2a323f49
Unknown <alopex@ymail.com>
2018-10-16 23:51:55 -0600

    Data Value math

--- 1610/Assets/Scripts/Powerup.cs
+++ 1610/Assets/Scripts/Powerup.cs
@@ -10,4 +10,15 @@ public class ValueChange : ScriptableObject {
         data.Value += Data.Value;        
     }
 
+    public void SubValue(FloatData data){
+        data.Value -= Data.Value;
+    }
+
+    public void MultiplyValue(FloatData data){
+        data.Value *= Data.Value;
+    }
+
+    public void DivideValue(FloatData data){
+        data.Value /= Data.Value;
+    }
 }
